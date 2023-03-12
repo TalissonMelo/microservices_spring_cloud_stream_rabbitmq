ec04acaf490ff2934bfc9a92fe88c96921f5b2b1
TalissonMelo <talissonmelorodrigues@gmail.com>
2023-03-12 20:40:04 -0300

    Criando os consumers e Exchanges e Queues em projeto scheduler

---
+++ scheduler/src/main/java/com/algaworks/example/spring/cloud/stream/scheduler/listener/AppCreatedEventListener.java
@@ -0,0 +1,31 @@
+package com.algaworks.example.spring.cloud.stream.scheduler.listener;
+
+import java.util.function.Consumer;
+
+import org.springframework.stereotype.Component;
+
+import com.algaworks.example.spring.cloud.stream.scheduler.api.model.AppModel;
+import com.algaworks.example.spring.cloud.stream.scheduler.domain.model.App;
+import com.algaworks.example.spring.cloud.stream.scheduler.domain.repository.AppRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class AppCreatedEventListener implements Consumer<AppModel> {
+
+    private final AppRepository apps;
+
+    @Override
+    public void accept(AppModel appModel) {
+        log.info("App criado recebido " + appModel.getId());
+        App app = App.builder()
+                .id(appModel.getId())
+                .name(appModel.getName())
+                .address(appModel.getAddress())
+                .build();
+        apps.saveAndFlush(app);
+    }
+}
