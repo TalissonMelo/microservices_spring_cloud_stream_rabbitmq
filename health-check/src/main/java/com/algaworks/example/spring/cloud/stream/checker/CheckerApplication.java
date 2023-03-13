8fbb312305235ac05269aac66e4d497375e31db3
TalissonMelo <talissonmelorodrigues@gmail.com>
2023-03-13 10:41:39 -0300

    Registrando demais microservices em eureka

--- health-check/src/main/java/com/algaworks/example/spring/cloud/stream/checker/CheckerApplication.java
+++ health-check/src/main/java/com/algaworks/example/spring/cloud/stream/checker/CheckerApplication.java
@@ -2,8 +2,10 @@ package com.algaworks.example.spring.cloud.stream.checker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CheckerApplication {
 
 	public static void main(String[] args) {
